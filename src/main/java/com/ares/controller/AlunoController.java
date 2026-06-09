0693076d5960f5aa5f98bcbb6c1d1dc691fcf288
BrunaSS-18 <bestudante206@gmail.com>
2026-06-09 11:29:33 -0300

    feat: adicionando API de IA

--- src/main/java/com/ares/controller/AlunoController.java
+++ src/main/java/com/ares/controller/AlunoController.java
@@ -4,6 +4,9 @@ import com.ares.exception.CpfJaCadastradoException;
 import com.ares.model.Aluno;
 import com.ares.repository.AlunoRepository;
 import com.ares.service.AlunoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,40 +17,49 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping ("/alunos")
+@Tag(name = "Alunos", description = "Endpoints de gerenciamento de alunos da academia")
 public class AlunoController {
 
     @Autowired
     private AlunoService alunoService;
 
     @GetMapping
+    @Operation (summary = "Listar todos os alunos", description = "Retorna uma lista complesta com todos os alunos cadastrados")
     public List<Aluno> listarTodos() {
         return alunoService.listarTodos();
     }
 
     @PostMapping
+    @Operation (summary = "Cadastrar novos alunos", description = "Cadastrar um aluno na academia")
+    @ApiResponse(responseCode = "200", description = "Retorna o aluno salvo")
+    @ApiResponse(responseCode = "409", description = "CPF já cadastrado")
     public ResponseEntity<Aluno> criarAluno(@Valid @RequestBody Aluno aluno) {
         Aluno salvo = alunoService.criarAluno(aluno);
         return ResponseEntity.status(201).body(salvo);
     }
 
     @PutMapping("/{id}")
+    @Operation (summary = "Atualizar alunos", description = "Atualiza todos os dados de um aluno existente")
     public ResponseEntity<Aluno> atualizar(@PathVariable Long id, @Valid @RequestBody Aluno aluno){
         Aluno atualizado = alunoService.atualizar(id, aluno);
         return ResponseEntity.ok(atualizado);
     }
 
     @DeleteMapping("/{id}")
+    @Operation (summary = "Deletar aluno", description = "Deletar aluno existente")
     public ResponseEntity<Void> deletar(@PathVariable Long id){
         alunoService.deletar(id);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{id}")
+    @Operation (summary = "Buscar um aluno por id", description = "Busca usuário existente por id")
     public ResponseEntity<Aluno> buscarPorId(@PathVariable Long id){
         return ResponseEntity.ok(alunoService.buscarPorId(id));
     }
 
     @GetMapping("/matricula/{id}")
+    @Operation (summary = "Verificar status da matricula", description = "Verifica se a matricula está ativa")
     public ResponseEntity<String> matriculaAtiva(@PathVariable Long id){
         Aluno aluno = alunoService.buscarPorId(id);
 
